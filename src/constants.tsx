b16a7fa7c64d556d0a1068f1c1dee0bf4b7c46c0
Emepi <elmeri.kemppainen@gmail.com>
2025-10-19 14:57:54 +0300

    Updated court args

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,11 +1,11 @@
-export const DEVNET_PACKAGE_ID = "0xb4c12618c09a0b54196c79ab8b76e92574d73ccdf8ef27f241eaddcb7aea09e5";
+export const DEVNET_PACKAGE_ID = "0x015327db098fb51888c2277b4fd9feabc869f7241f9d2db668d183d8ae5e93fb";
 export const TESTNET_PACKAGE_ID = "0x1234"; // not deployed
 export const MAINNET_PACKAGE_ID = "0x1234"; // not deployed
 
-export const DEVNET_REGISTRY_ID = "0xa6dd0d2c0c2f0a858499f997a72d1016dab5af4ad1974c588a588ceea52451ad";
+export const DEVNET_REGISTRY_ID = "0x10b0b94b6dfe4444e286973914692ee78a368fc6a84c9b253b1a785153c40cd2";
 export const TESTNET_REGISTRY_ID = "0x1234"; // not deployed
 export const MAINNET_REGISTRY_ID = "0x1234"; // not deployed
 
-export const DEVNET_COIN_TYPE = "0xb4c12618c09a0b54196c79ab8b76e92574d73ccdf8ef27f241eaddcb7aea09e5::nvr::NVR";
+export const DEVNET_COIN_TYPE = "0x15327db098fb51888c2277b4fd9feabc869f7241f9d2db668d183d8ae5e93fb::nvr::NVR";
 export const TESTNET_COIN_TYPE = "0x1234"; // not deployed
 export const MAINNET_COIN_TYPE = "0x1234"; // not deployed
